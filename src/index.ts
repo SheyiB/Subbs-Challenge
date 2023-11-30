c466121b1390f06f78fe17bf3eb26b986ca46d87
Seyi-Banjo <elijahbanjo@gmail.com>
2023-11-30 15:00:00 +0100

    Feature:Server-Setup

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,29 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
+import express from "express"
 import { Request, Response } from "express"
 import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
-import { User } from "./entity/User"
+import {userRouter} from "./routers/user.routes"
+import {authRouter} from "./routers/auth.routes"
+import swaggerUi from "swagger-ui-express"
 
-AppDataSource.initialize().then(async () => {
+AppDataSource.initialize()
+    .then(async () => {console.log("Connected to database")})
+    .catch(error => console.log(error))
 
-    // create express app
-    const app = express()
-    app.use(bodyParser.json())
+export const app = express()
 
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+app.use(express.json())
 
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
+app.use('/api/tasks', userRouter)
+app.use('/api/auth', authRouter )
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(require("../swagger.json")))
+app.all('*', (req: Request, res: Response) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
     })
+})
 
-    // setup express app here
-    // ...
+const PORT = process.env.PORT || 8080
 
-    // start express server
-    app.listen(3000)
+app.listen(PORT, ()=>{console.log(`Server running on port ${PORT}`)})
 
-    // insert new users for test
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 27
-        })
-    )
-
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Phantom",
-            lastName: "Assassin",
-            age: 24
-        })
-    )
-
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
-
-}).catch(error => console.log(error))
